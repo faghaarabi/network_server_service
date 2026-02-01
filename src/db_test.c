aac2df83c463e876fea00d4ae9d372a855ce3309
faghaarabi <faghaarabi@my.bcit.ca>
2026-01-31 22:30:44 -0800

    Update db_test.c

--- src/db_test.c
+++ src/db_test.c
@@ -12,3 +12,18 @@ static void dump(const char *username, const void *value, size_t len, void *ctx)
     }
     printf("\n");
 }
+
+#include "user_db.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void dump(const char *username, const void *value, size_t len, void *ctx) {
+    (void)ctx;
+    printf("user=%s len=%zu ", username, len);
+    if (value && len) {
+        printf("value=");
+        fwrite(value, 1, len, stdout);
+    }
+    printf("\n");
+}
