6bbdb0c9236f59a96bbcb1f62bc51e9dba4486f8
faghaarabi <faghaarabi@my.bcit.ca>
2026-01-31 22:27:59 -0800

    Modify user_db.h

--- include/user_db.h
+++ include/user_db.h
@@ -30,3 +30,34 @@ extern "C" {
 
     /** Return true if username exists */
     udb_status_t user_db_exists(user_db_t *db, const char *username, bool *out_exists);
+
+
+    /**
+ * Store a user record (value is arbitrary bytes).
+ * overwrite=false will fail with UDB_ERR_EXISTS if user already exists.
+ */
+    udb_status_t user_db_put(user_db_t *db,
+                             const char *username,
+                             const void *value,
+                             size_t value_len,
+                             bool overwrite);
+
+    /**
+     * Fetch a user record (value bytes are malloc'ed; caller must free).
+     */
+    udb_status_t user_db_get(user_db_t *db,
+                             const char *username,
+                             void **out_value,
+                             size_t *out_len);
+
+    /** Delete user */
+    udb_status_t user_db_del(user_db_t *db, const char *username);
+
+    /**
+     * Optional: iterate all users (debug/admin/testing)
+     * callback called once per key/value
+     */
+    typedef void (*user_db_iter_cb)(const char *username,
+                                   const void *value,
+                                   size_t value_len,
+                                   void *ctx);
