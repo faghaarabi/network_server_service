6e173139cf95da86a15a10fe224c15fae324b019
faghaarabi <faghaarabi@my.bcit.ca>
2026-01-31 22:37:10 -0800

    Remove dump()

--- src/db_test.c
+++ src/db_test.c
@@ -1,17 +1,3 @@
-#include "user_db.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-static void dump(const char *username, const void *value, size_t len, void *ctx) {
-    (void)ctx;
-    printf("user=%s len=%zu ", username, len);
-    if (value && len) {
-        printf("value=");
-        fwrite(value, 1, len, stdout);
-    }
-    printf("\n");
-}
 
 #include "user_db.h"
 #include <stdio.h>
