64cecddb34c557dcc990696b4b9f3f4436def4ce
faghaarabi <faghaarabi@my.bcit.ca>
2026-01-31 22:26:27 -0800

    Update user_db.h

--- include/user_db.h
+++ include/user_db.h
@@ -8,3 +8,13 @@
 extern "C" {
 #endif
 
+    typedef struct user_db user_db_t;
+
+    typedef enum {
+        UDB_OK = 0,
+        UDB_ERR_INVALID = -1,
+        UDB_ERR_OPEN = -2,
+        UDB_ERR_IO = -3,
+        UDB_ERR_NOTFOUND = -4,
+        UDB_ERR_EXISTS = -5
+    } udb_status_t;
