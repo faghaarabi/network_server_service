be262bc3d71ed18a4461e6212c8b785ab693dd88
faghaarabi <faghaarabi@my.bcit.ca>
2026-02-04 21:24:43 -0800

    Add protocol handler skeleton and message type definitions

--- protocol.c
+++ protocol.c
@@ -1,3 +1,34 @@
-//
-// Created by Fereshteh on 2/4/26.
-//
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>   // ntohl/htonl
+
+#include "user_db.h"
+
+// ---- constants (match your ASN.1 / hexpat numeric values) ----
+enum { PROTO_V1 = 0x01 };
+
+enum MsgType {
+    MSG_SERVER_REG_REQ   = 0x00,
+    MSG_SERVER_REG_RES   = 0x01,
+
+    MSG_HEALTH_REQ       = 0x04,
+    MSG_HEALTH_RES       = 0x05,
+
+    MSG_ACCT_REG_REQ     = 0x10,
+    MSG_ACCT_REG_RES     = 0x11,
+
+    MSG_LOGIN_LOGOUT_REQ = 0x14,
+    MSG_LOGIN_LOGOUT_RES = 0x15,
+
+    MSG_LOG_REQ          = 0x18,
+    MSG_LOG_RES          = 0x19
+};
+
+enum Status {
+    ST_OK = 0,
+    ST_SENDER_ERR = 1,
+    ST_RECEIVER_ERR = 2
+};
